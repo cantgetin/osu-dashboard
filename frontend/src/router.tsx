0e443c95321d0c12370faa39f04574f2d6c82604
cantgetin <84032933+cantgetin@users.noreply.github.com>
2025-05-10 00:25:57 +0500

    fix(front): use rrd ScrollRestoration

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,39 +1,51 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Outlet, ScrollRestoration} from "react-router-dom";
+import Main from "./pages/Main.tsx";
 import User from "./pages/User.tsx";
 import Beatmapset from "./pages/Beatmapset.tsx";
 import Users from "./pages/Users.tsx";
 import Beatmapsets from "./pages/Beatmapsets.tsx";
-import Main from "./pages/Main.tsx";
-import NotFound from "./pages/NotFound.tsx";
 import Authorize from "./pages/Authorize.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 export const router = createBrowserRouter([
     {
-        path: "/",
-        element: <Main/>,
-    },
-    {
-        path: "/user/:userId",
-        element: <User/>,
-    },
-    {
-        path: "/beatmapset/:mapId",
-        element: <Beatmapset/>,
-    },
-    {
-        path: "/users",
-        element: <Users/>,
-    },
-    {
-        path: "/beatmapsets",
-        element: <Beatmapsets/>,
-    },
-    {
-        path: "/authorize",
-        element: <Authorize/>,
-    },
-    {
-        path: "*",
-        element: <NotFound />,
-    },
+        element: (
+            <>
+                <ScrollRestoration/>
+                <Outlet/>
+            </>
+        ),
+        children: [
+            {
+                path: "/",
+                element: <Main/>,
+            },
+            {
+                path: "/user/:userId",
+                element: <User/>,
+            },
+            {
+                path: "/beatmapset/:mapId",
+                element: <Beatmapset/>,
+            },
+            {
+                path: "/users",
+                element: <Users/>,
+            },
+            {
+                path: "/beatmapsets",
+                element: <Beatmapsets/>,
+            },
+            {
+                path: "/authorize",
+                element: <Authorize/>,
+            },
+            {
+                path: "*",
+                element: <NotFound/>,
+            },
+        ]
+    }
+
+
 ]);
